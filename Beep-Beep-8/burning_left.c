c378e08d7c70a9b49dbe997dfcdf7a962bdc3668
perryrobotics <47753315+perryrobotics@users.noreply.github.com>
2019-02-20 19:29:31 -0600

    Update burning_left.c

--- Beep-Beep-8/burning_left.c
+++ Beep-Beep-8/burning_left.c
@@ -4,16 +4,20 @@
 void burning_left()
 
 {
+    //              STAGE 1, AMBULANCE              //
     right(900, 500);
     forward(1100, 1500);
     arm_down();
     claw_open();
     arm_up();
+    // Stage 1 - Ambulance is complete! //
     // Onward for the fire truck! //
     
+    //             STAGE 2, FIRE TRUCK              //
     backward(1100, 6000);
     right(900,200);
     backward(900,560);
+    // back of pipe //
     forward(1000, 1700);
     left(1200, 700);   
     drive_to_black(0, THRESH);
@@ -23,22 +27,27 @@ void burning_left()
     forward(900, 1000);
     claw_close();
     arm_up();
-    // firetruck scored!, get fireman #1 //
+    // firetruck grabbed! //
     
     backward(900, 200);
     right(900, 200);
     drive_to_black(0, THRESH);
+    
     backward(800,50);
     arm_down();
     claw_open();
     arm_up();
+    // fire truck scored!, off for fireman #1 //
+    
+    //             STAGE 3, FIRST FIREMAN              //
     backward(900, 900);
     right(900,200);
     backward_time(1400, 3500);
-    //back into pipe
+    // back into pipe //
     forward(1000, 2400);
     left(1000, 850);
     drive_to_black(0, THRESH);
+    
     backward(800, 200);
     arm_down();
     forward(900, 700);
@@ -51,13 +60,9 @@ void burning_left()
     drive_to_black(0, THRESH);   
     left(900, 800);
     forward(900, 2200);
-    claw_open();  
-    //  first fireman scored! //
-
-
-    ///////////////////////////////////////////////////////
-    //		    	GO GET SECOND FIREMAN!!!             //
-    ///////////////////////////////////////////////////////
+    claw_open();  //  first fireman scored! //
+    
+    //              STAGE 4, SECOND FIREMAN              //
     backward(900, 1000);
     right(900, 600);
     backward_time(1200, 4500); // hit back of pipe
@@ -68,12 +73,14 @@ void burning_left()
     forward(900, 200);
     claw_close();
     backward(900, 250);
-    right(900, 500); // score second fireman!!!
-    claw_open();
+    right(900, 500); 
+    claw_open(); // score second fireman!! //
     arm_up();
-
-    ///////////////////////////////////////////////////////// 
-    backward_time(900, 2000); // hit back of pipe!
+    // Stage 4 - Second Fireman is complete! //
+    
+    //              STAGE 5, THIRD FIREMAN              //
+    backward_time(900, 2000); 
+    // hit back of pipe!
     right(900, 200);
     backward_time(1000, 2000);
     forward(1000, 2300);
@@ -83,12 +90,12 @@ void burning_left()
     forward(900, 200);
     claw_close();
     backward(900, 200);
-    right(900, 500); // score third fireman!!!
-    claw_open();
+    right(900, 500); 
+    claw_open(); // score third fireman!!! //
     arm_up();
-
-    ////////////////////////////////////////////////////////////////
-   
+    // Stage 5 - Third Fireman is complete! //
+    
+    //              STAGE 6, FOURTH FIREMAN              //
     backward_time(900, 2500);
     right(900, 600);
     backward_time(1200, 1500); // hit back of pipe
@@ -99,21 +106,28 @@ void burning_left()
     forward(900, 200);
     claw_close();
     backward(900, 200);
-    right(900, 500); // score fourth fireman!!!
-    claw_open();
+    right(900, 500); 
+    claw_open(); // score fourth fireman!!!! //
     arm_up();
     claw_close();
-    ///////////////////////////
+    // Stage 6 - Fourth Fireman is complete! //
+    
+    //              STAGE 7, FIFTH FIREMAN              //
     left(900,750);
     arm_down();
     forward(900,200);
-    right(900,580);
-    arm_up();
+    right(900,580); // score fifth fireman!!!!! //
+    arm_up(); 
+    // Stage 7 - Fifth Fireman is complete! //
+    
+    //              STAGE 8, FINAL ALIGNMENT              //
     right(900, 600);
     forward(900,100);
     arm_down();
     left(300, 300);
-
+    // Stage 8 - Final Alignment is complete! //
+    
+    //       BEEP BEEP 8 BURNING_LEFT.C IS COMPLETE       //
     
 
 }   
